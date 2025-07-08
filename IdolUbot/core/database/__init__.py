bd6b5891656e8c583789ba44f49434da66d5a9c3
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-07-08 16:00:22 +0700

    .

--- IdolUbot/core/database/__init__.py
+++ IdolUbot/core/database/__init__.py
@@ -12,39 +12,39 @@ ubot_collection = mongodb["idol"]
 vars_collection = mongodb["varsX"]
 
 # Fungsi dari setiap modul (dengan absolute import)
-from IdolUbot.core.database.expired import (
-    get_expired_date,
-    set_expired_date,
-    rem_expired_date,
-)
+# from IdolUbot.core.database.expired import (
+#     get_expired_date,
+#     set_expired_date,
+#     rem_expired_date,
+# )
 
-from IdolUbot.core.database.pref import (
-    get_pref,
-    set_pref,
-    rem_pref,
-)
+# from IdolUbot.core.database.pref import (
+#     get_pref,
+#     set_pref,
+#     rem_pref,
+# )
 
-from IdolUbot.core.database.userbot import (
-    add_ubot,
-    remove_ubot,
-    get_userbots,
-)
+# from IdolUbot.core.database.userbot import (
+#     add_ubot,
+#     remove_ubot,
+#     get_userbots,
+# )
 
-from IdolUbot.core.database.variabel import (
-    set_vars,
-    get_vars,
-    remove_vars,
-    all_vars,
-    remove_all_vars,
-    get_list_from_vars,
-    add_to_vars,
-    remove_from_vars,
-    get_pm_id,
-    add_pm_id,
-    remove_pm_id,
-    set_status,
-    get_status,
-)
+# from IdolUbot.core.database.variabel import (
+#     set_vars,
+#     get_vars,
+#     remove_vars,
+#     all_vars,
+#     remove_all_vars,
+#     get_list_from_vars,
+#     add_to_vars,
+#     remove_from_vars,
+#     get_pm_id,
+#     add_pm_id,
+#     remove_pm_id,
+#     set_status,
+#     get_status,
+# )
 
 # daftar semua yang bisa diakses langsung
 __all__ = [
