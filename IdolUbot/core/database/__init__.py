636fc0d785980539066d82622093e087378bffea
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-07-08 15:57:45 +0700

    .

--- IdolUbot/core/database/__init__.py
+++ IdolUbot/core/database/__init__.py
@@ -46,43 +46,43 @@ from IdolUbot.core.database.variabel import (
     get_status,
 )
 
-# daftar semua yang bisa diakses langsung
-__all__ = [
-    # MongoDB
-    "mongo_client",
-    "mongodb",
-    "user_expired_collection",
-    "prefix_collection",
-    "ubot_collection",
-    "vars_collection",
+# # daftar semua yang bisa diakses langsung
+# __all__ = [
+#     # MongoDB
+#     "mongo_client",
+#     "mongodb",
+#     "user_expired_collection",
+#     "prefix_collection",
+#     "ubot_collection",
+#     "vars_collection",
 
-    # expired.py
-    "get_expired_date",
-    "set_expired_date",
-    "rem_expired_date",
+#     # expired.py
+#     "get_expired_date",
+#     "set_expired_date",
+#     "rem_expired_date",
 
-    # pref.py
-    "get_pref",
-    "set_pref",
-    "rem_pref",
+#     # pref.py
+#     "get_pref",
+#     "set_pref",
+#     "rem_pref",
 
-    # userbot.py
-    "add_ubot",
-    "remove_ubot",
-    "get_userbots",
+#     # userbot.py
+#     "add_ubot",
+#     "remove_ubot",
+#     "get_userbots",
 
-    # variabel.py
-    "set_vars",
-    "get_vars",
-    "remove_vars",
-    "all_vars",
-    "remove_all_vars",
-    "get_list_from_vars",
-    "add_to_vars",
-    "remove_from_vars",
-    "get_pm_id",
-    "add_pm_id",
-    "remove_pm_id",
-    "set_status",
-    "get_status",
-]
+#     # variabel.py
+#     "set_vars",
+#     "get_vars",
+#     "remove_vars",
+#     "all_vars",
+#     "remove_all_vars",
+#     "get_list_from_vars",
+#     "add_to_vars",
+#     "remove_from_vars",
+#     "get_pm_id",
+#     "add_pm_id",
+#     "remove_pm_id",
+#     "set_status",
+#     "get_status",
+# ]
