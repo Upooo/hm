75ee98f01239806ad3a5706c12e53f116e92f0f9
Arya Septian Anggara <aryasptnanggra@gmail.com>
2025-07-08 16:39:59 +0700

    .

--- IdolUbot/core/function/plugins.py
+++ IdolUbot/core/function/plugins.py
@@ -5,7 +5,7 @@ from pyrogram import __version__
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 from IdolUbot import *
-from IdolUbot.config import LOGS_ON_UBOT
+from IdolUbot.config import LOGS_MAKER_UBOT
 from IdolUbot.modules import loadModule
 from IdolUbot.core.helpers import PY
 
@@ -20,28 +20,31 @@ async def loadPlugins() -> None:
             HELP_COMMANDS[module_name] = imported
 
     print(f"[🤖 {bot.me.full_name} 🤖] [💠 ᴘʟᴜɢɪɴꜱ ʙᴇʀʜᴀꜱɪʟ ᴅɪ ᴍᴜᴀᴛ 💠]")
+    for chat_id in LOGS_MAKER_UBOT:
+        try:
+            await bot.send_message(
+                chat_id,
+                f"""<blockquote><b><u>» {bot.me.mention} sᴛᴀʀᴛᴇᴅ :</u></b>
 
-    await bot.send_message(
-        LOGS_ON_UBOT,
-        f"""<blockquote><b><u>» {bot.me.mention} sᴛᴀʀᴛᴇᴅ :</u></b>
-
-<b>ɪᴅ :</b> <code>{bot.me.id}</code>
-<b>ɴᴀᴍᴇ :</b> <b>{bot.me.full_name}</b>
-<b>ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs :</b> <code>{len(HELP_COMMANDS)}</code>
-<b>ᴛᴏᴛᴀʟ ᴘᴇɴɢɢᴜɴᴀ :</b> <code>{len(ubot._ubot)}</code>
-<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{bot.me.username}
-</blockquote>""",
-        reply_markup=InlineKeyboardMarkup([
-            [
-                
-                InlineKeyboardButton("📦 ꜱᴛᴏʀᴇ", url="https://t.me/+op4OV4pI2Kk4ZGU1"),
-                InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/nathanidol"),
-            ],
-            [
-                InlineKeyboardButton("🤖 ᴜsᴇʀʙᴏᴛ", url=f"https://t.me/{bot.me.username}"),
-            ],
-        ]),
-    )
+        <b>ɪᴅ :</b> <code>{bot.me.id}</code>
+        <b>ɴᴀᴍᴇ :</b> <b>{bot.me.full_name}</b>
+        <b>ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs :</b> <code>{len(HELP_COMMANDS)}</code>
+        <b>ᴛᴏᴛᴀʟ ᴘᴇɴɢɢᴜɴᴀ :</b> <code>{len(ubot._ubot)}</code>
+        <b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{bot.me.username}
+        </blockquote>""",
+                reply_markup=InlineKeyboardMarkup([
+                    [
+                        
+                        InlineKeyboardButton("📦 ꜱᴛᴏʀᴇ", url="https://t.me/+op4OV4pI2Kk4ZGU1"),
+                        InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/nathanidol"),
+                    ],
+                    [
+                        InlineKeyboardButton("🤖 ᴜsᴇʀʙᴏᴛ", url=f"https://t.me/{bot.me.username}"),
+                    ],
+                ]),
+            )
+        except Exception as e:
+            print(f"GAGAL KIRIM KE {chat_id} : {e}")
 
 @PY.CALLBACK("0_cls")
 async def close_callback(client, callback_query: CallbackQuery) -> None:
